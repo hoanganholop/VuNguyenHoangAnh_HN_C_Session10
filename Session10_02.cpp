dfcd68226ac50af283d9661a541895a1f535647e
hoanganholop <hoanganholop@gmail.com>
2024-12-02 23:23:11 +0700

    Add files via upload

---
+++ Session10_02.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main() {
+	int arr[] = { 1,7,5,8,4,9,2 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 0;i < n - 1;i++) {
+		for (int j = 0;j < n - i - 1;j++) {
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	printf("Mang sau khi duoc sap xep la:");
+	for (int i = 0;i < n;i++) {
+		printf("%d\t", arr[i]);
+	}
+	return 0;
+}
